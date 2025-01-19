71555a85940a58b0545b48ee1b897ae4a92507a8
Jesusboy-001 <olamidejunior231@gmail.com>
2025-01-19 19:22:28 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const version = require("@whiskeysockets/baileys/package.json").version
 
 
 //Input number for pair code
-global.pairingNumber = "2349159626482"
+global.pairingNumber = "2348071284970"
 
 
 
